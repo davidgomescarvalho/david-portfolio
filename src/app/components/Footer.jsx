0b369ed2f955f6a52c359eb81f9daa52329ecf8b
david gomes <davidgomesk8@gmail.com>
2024-10-08 21:32:03 +0200

    footer updated

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import Image from 'next/image'
-import MainLogo from '../../../public/images/logos/david-logo.png'
+import React from "react";
+import Image from "next/image";
+import MainLogo from "../../../public/images/logos/david-logo.png";
 
 const Footer = () => {
   return (
-    <footer className='footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
-      <div className='container p-12 flex justify-between'>
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
         <span>
-          <Image
-            src={MainLogo}
-            alt='Logo'
-            width={180}
-            height={50}
-          />
+          <Image src={MainLogo} alt="Logo" width={180} height={50} />
         </span>
-        <p className='text-slate-600'>All rights reserved</p>
+        <p className="text-slate-600">2024 - Made with Next.js</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
