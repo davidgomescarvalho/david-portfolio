75d51ebbb0228e3441bacb8c45d2acd76404a4fd
david gomes <davidgomesk8@gmail.com>
2024-11-27 16:56:39 +0100

    fixed footer z-index and hided green gradient

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,22 +1,21 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client";
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import GithubIcon from '../../../public/github-logo.svg';
-import LinkedInIcon from '../../../public/linkedin-logo.svg';
-import Ruby from '../../../public/images/logos/ruby.png';
-import Rails from '../../../public/images/logos/rails.png';
-import JavaScript from '../../../public/images/logos/js.png';
-import ReactLogo from '../../../public/images/logos/react.png';
-import Mongo from '../../../public/images/logos/mongo.png';
-import Bootstrap from '../../../public/images/logos/bootstrap.png';
-import Figma from '../../../public/images/logos/figma.png';
-import Tailwind from '../../../public/images/logos/tailwind.png';
+import GithubIcon from "../../../public/github-logo.svg";
+import LinkedInIcon from "../../../public/linkedin-logo.svg";
+import Ruby from "../../../public/images/logos/ruby.png";
+import Rails from "../../../public/images/logos/rails.png";
+import JavaScript from "../../../public/images/logos/js.png";
+import ReactLogo from "../../../public/images/logos/react.png";
+import Mongo from "../../../public/images/logos/mongo.png";
+import Bootstrap from "../../../public/images/logos/bootstrap.png";
+import Figma from "../../../public/images/logos/figma.png";
+import Tailwind from "../../../public/images/logos/tailwind.png";
 
-
-import Link from 'next/link';
-import Image from 'next/image';
-import { px } from 'framer-motion';
+import Link from "next/link";
+import Image from "next/image";
+import { px } from "framer-motion";
 
 const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
@@ -26,44 +25,42 @@ const EmailSection = () => {
     const data = {
       email: e.target.email.value,
       subject: e.target.subject.value,
-      message: e.target.message.value
-    }
+      message: e.target.message.value,
+    };
     const JSONdata = JSON.stringify(data);
-    const endpoint = 'api/send';
+    const endpoint = "api/send";
 
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSONdata,
-    }
+    };
     const response = await fetch(endpoint, options);
     const resData = await response.json();
     if (response.status === 200) {
       console.log("Message sent successfully");
       setEmailSubmitted(true);
     }
-  }
+  };
   return (
-    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative' id="contact">
-      <div
-        className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-12 -translate-y-12'></div>
-      <div className='z-10'>
-        <h5 className='text-xl font-bold text-white my-2'>
-          Let's Connect
-        </h5>
-        <p className='text-[#ADB7BE] mb-4 max-w-md'>
-          Hello!
-          I'm currently looking for new opportunities, my inbox is always open.
-          Whether you have a question or contact me, I'll quickly get back to you!
+    <section
+      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+      id="contact"
+    >
+      {/* <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-12 -translate-y-12"></div> */}
+      <div className="">
+        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <p className="text-[#ADB7BE] mb-4 max-w-md">
+          Hello! I'm currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or contact me, I'll quickly get back
+          to you!
         </p>
-        <p className='text-[#ADB7BE] mb-4 max-w-md'>
+        <p className="text-[#ADB7BE] mb-4 max-w-md">
           Email: david.gomes.web@gmail.com
         </p>
-        <p className='text-[#ADB7BE] mb-4 max-w-md'>
-          Phone: +33 6 98 51 44 11
-        </p>
+        <p className="text-[#ADB7BE] mb-4 max-w-md">Phone: +33 6 98 51 44 11</p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/davidgomescarvalho">
             <Image src={GithubIcon} alt="Github Icon" />
@@ -71,7 +68,6 @@ const EmailSection = () => {
           <Link href="https://www.linkedin.com/in/david-gomes-24548031a/">
             <Image src={LinkedInIcon} alt="LinkedIn Icon" />
           </Link>
-
         </div>
       </div>
       <div>
@@ -161,7 +157,7 @@ const EmailSection = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default EmailSection
+export default EmailSection;
